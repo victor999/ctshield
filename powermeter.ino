446ac2364d6bf4ecc4a740c698030d30cc683b57
victor999 <victor.vvs@gmail.com>
2015-12-10 23:52:12 +0200

    Update powermeter.ino

--- powermeter.ino
+++ powermeter.ino
@@ -15,9 +15,6 @@ ESP8266 wifi(Serial2, 115200);
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-
 #define NUM_OF_SENSORS 3
 
 EnergyMonitor emon[NUM_OF_SENSORS];                   // Create an energyMonitor instances
@@ -60,25 +57,6 @@ char g_sensorString[100] = "";
 
 //counter for loop
 unsigned int g_loopCounter = 0;
-
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ 
-  B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
 
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
